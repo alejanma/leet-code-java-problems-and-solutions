d355c076548483f77f12afe43ae6ddfbb8b8de24
alejanma <mar.ale.her2030@hotmail.com>
2024-06-19 14:32:02 -0400

    Second commit to view the project structure.

---
+++ src/MergeStringsAlternately/Main.java
@@ -0,0 +1,8 @@
+package MergeStringsAlternately;
+//Original Problem : https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=programming-skills
+public class Main {
+    public static void main (String[] args){
+
+        System.out.println("Hello");
+    }
+}
---
+++ src/MergeStringsAlternately_1768.java
@@ -0,0 +1,6 @@
+//Original Problem : https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=programming-skills
+public class MergeStringsAlternately_1768 {
+    public static void main (String[] args){
+        System.out.println("This is a leet code problem");
+    }
+}
